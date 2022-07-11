e14166e8c7c98eccacfad298a570b8b1366f850f
jobish <jobish@Jose>
2022-07-11 21:26:52 +0530

    commit

--- src/main/java/com/ilistener/ListenerTest.java
+++ src/main/java/com/ilistener/ListenerTest.java
@@ -1,38 +1,60 @@
 package com.ilistener;
 
+import org.testng.ITestContext;
 import org.testng.ITestListener;
+import org.testng.ITestResult;
 
 public class ListenerTest implements ITestListener{
 
 	@Override
-	public int hashCode() {
+	public void onTestStart(ITestResult result) {
 		// TODO Auto-generated method stub
-		return super.hashCode();
+		ITestListener.super.onTestStart(result);
 	}
 
 	@Override
-	public boolean equals(Object obj) {
+	public void onTestSuccess(ITestResult result) {
 		// TODO Auto-generated method stub
-		return super.equals(obj);
+		ITestListener.super.onTestSuccess(result);
 	}
 
 	@Override
-	protected Object clone() throws CloneNotSupportedException {
+	public void onTestFailure(ITestResult result) {
 		// TODO Auto-generated method stub
-		return super.clone();
+		ITestListener.super.onTestFailure(result);
 	}
 
 	@Override
-	public String toString() {
+	public void onTestSkipped(ITestResult result) {
 		// TODO Auto-generated method stub
-		return super.toString();
+		ITestListener.super.onTestSkipped(result);
 	}
 
 	@Override
-	protected void finalize() throws Throwable {
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
 		// TODO Auto-generated method stub
-		super.finalize();
+		ITestListener.super.onTestFailedButWithinSuccessPercentage(result);
 	}
+
+	@Override
+	public void onTestFailedWithTimeout(ITestResult result) {
+		// TODO Auto-generated method stub
+		ITestListener.super.onTestFailedWithTimeout(result);
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+		ITestListener.super.onStart(context);
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+		// TODO Auto-generated method stub
+		ITestListener.super.onFinish(context);
+	}
+
+	
 	
 }
 
